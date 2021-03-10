416e211fdf4e314248fffd4985e4880d7451a0ae
Desmond <b22191774@gmail.com>
2021-03-10 17:46:22 +0800

    work0310

--- js/myJquery.js
+++ js/myJquery.js
@@ -1,9 +1,12 @@
 $('.box').mousemove(function (event) {
     event.preventDefault();
-     var upX = event.clientX;
-     var upY = event.clientY;
-     console.log(upX, upY);
-     $('.sunBox').css("height",upY);
-     $('.bar').css("top",upY-5);
+    var upX = event.clientX;
+    var upY = event.clientY;
+    console.log(upX, upY);
+    $('.sunBox').css("height", upY);
+    if (upY <= 476) {
+        $('.bar').css("top", upY - 5);
+    }
 
- });
+
+});
